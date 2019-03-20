610445a728c5b02a8a408d106abca2d0461dd5fe
Paulo Olayres <pjolayres@live.com>
2019-03-20 14:36:10 +0400

    Updated to Next.s 8

--- next.config.js
+++ next.config.js
@@ -9,5 +9,6 @@ module.exports = withSass({
     };
 
     return config;
-  }
+  }// ,
+  // target: 'serverless'
 });
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Link from 'next/link'
-import Head from '../components/common/head'
-import Nav from '../components/client/nav'
+import React from 'react';
+import Link from 'next/link';
+import Head from '../components/common/head';
+import Nav from '../components/client/nav';
 
 const Home = () => (
   <div>
@@ -11,7 +11,9 @@ const Home = () => (
     <div className="hero">
       <h1 className="title">Welcome to Next!</h1>
       <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
+        To get started, edit
+        <code>pages/index.js</code>
+        and save to reload.
       </p>
 
       <div className="row">
@@ -25,8 +27,9 @@ const Home = () => (
           <a className="card">
             <h3>Examples &rarr;</h3>
             <p>
-              Find other example boilerplates on the{' '}
-              <code>create-next-app</code> site
+              Find other example boilerplates on the
+              <code>create-next-app</code>
+              site
             </p>
           </a>
         </Link>
@@ -39,57 +42,59 @@ const Home = () => (
       </div>
     </div>
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
+    <style jsx>
+      {`
+        .hero {
+          width: 100%;
+          color: #333;
+        }
+        .title {
+          margin: 0;
+          width: 100%;
+          padding-top: 80px;
+          line-height: 1.15;
+          font-size: 48px;
+        }
+        .title,
+        .description {
+          text-align: center;
+        }
+        .row {
+          max-width: 880px;
+          margin: 80px auto 40px;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-around;
+        }
+        .card {
+          padding: 18px 18px 24px;
+          width: 220px;
+          text-align: left;
+          text-decoration: none;
+          color: #434343;
+          border: 1px solid #9b9b9b;
+        }
+        .card:hover {
+          border-color: #067df7;
+        }
+        .card h3 {
+          margin: 0;
+          color: #067df7;
+          font-size: 18px;
+        }
+        .card p {
+          margin: 0;
+          padding: 12px 0 0;
+          font-size: 13px;
+          color: #333;
+        }
+      `}
+    </style>
   </div>
-)
+);
 
-Home.getInitialProps = ({ res, err }) => ({
+Home.getInitialProps = () => ({
   namespacesRequired: ['common']
 });
 
-export default Home
+export default Home;
